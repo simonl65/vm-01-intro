3ade7eb7401c45959d3ab3c2e18db6d997ed9703
Simon Lincoln <simon.lincoln@contentdiscovered.com>
2018-09-28 11:21:11 +0100

    Added image and link

--- main.js
+++ main.js
@@ -4,5 +4,7 @@ var app = new Vue({
     data: {
         product: 'Socks',
         description: 'They go on your feet!',
+        image: './assets/vmSocks-green.jpg',
+        link: 'http://www.google.com',
     }
 });
