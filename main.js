0f3230d08f9ac8c9be7cabc1e112a952e21c24d7
Simon Lincoln <simon.lincoln@contentdiscovered.com>
2018-09-28 11:49:41 +0100

    Added display of variants

--- main.js
+++ main.js
@@ -6,5 +6,14 @@ var app = new Vue({
         image: './assets/vmSocks-green.jpg',
         inStock: true,
         details: ["80% cotton", "20% polyester", "Gender-neutral"],
-    }
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],    }
 });
