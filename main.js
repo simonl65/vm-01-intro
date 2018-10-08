2703ec19b3c18b380b6950eebe3b86ffbf1bd0e1
Simon Lincoln <simon.lincoln@contentdiscovered.com>
2018-10-08 09:44:18 +0100

    Challenge: Add line-through to Out Of Stock message if product is out of stock

--- main.js
+++ main.js
@@ -2,32 +2,32 @@ var app = new Vue({
     el: "#app",
 
     data: {
-    product: "Socks",
-    image: "./assets/vmSocks-green.jpg",
-    inStock: false,
-    details: ["80% cotton", "20% polyester", "Gender-neutral"],
-    variants: [
-        {
-            variantId: 2234,
-            variantColor: "green",
-            variantImage: "./assets/vmSocks-green.jpg"
-        },
-        {
-            variantId: 2235,
-            variantColor: "blue",
-            variantImage: "./assets/vmSocks-blue.jpg"
-        }
-    ],
-    cart: 0
+        product: "Socks",
+        image: "./assets/vmSocks-green.jpg",
+        inStock: false,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green",
+                variantImage: "./assets/vmSocks-green.jpg"
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue",
+                variantImage: "./assets/vmSocks-blue.jpg"
+            }
+        ],
+        cart: 0
     },
 
     methods: {
-    addToCart: function() {
-        this.cart += 1;
-    },
+        addToCart: function() {
+            this.cart += 1;
+        },
 
-    updateProduct: function(image) {
-        this.image = image;
-    }
+        updateProduct: function(image) {
+            this.image = image;
+        }
     }
 });
